adead77875f32431dc77471748df1bc1567f8eb4
gabrielbaute <gabrielbaute@gmail.com>
2026-09-07 13:29:06 -0400

    Implementación del modelo de respuesta

---
+++ lib/models/electric_event_response_model.dart
@@ -0,0 +1,41 @@
+import 'package:veneluz_app/enums/event_type_enum.dart';
+import 'package:veneluz_app/enums/fail_cause_enum.dart';
+
+///Modelo de respuesta de un nuevo evento eléctrico. USado cuando se consulta un evento.
+///
+/// Attributes:
+/// - `id` (String): ID de registro de la falla/evento eléctrico.
+/// - `startTimestamp` (DateTime): Marca de tiempo de inicio del evento.
+/// - `endTimestamp` (DateTime): Marca de tiempo de finalización del evento.
+/// - `location` (String): Coordenadas desde las que se registró el evento.
+/// - `evenType` (EventType): Tipo de evento, corte o fluctuación.
+/// - `failCause` (FailCause): Tipo de causa de la falla/corte.
+class ElectricEventResponse {
+  final String id;
+  final DateTime startTimestamp;
+  final DateTime? endTimestamp;
+  final String location;
+  final EventType evenType;
+  final FailCause failCause;
+
+  ElectricEventResponse({
+    required this.id,
+    required this.startTimestamp,
+    this.endTimestamp,
+    required this.location,
+    required this.evenType,
+    required this.failCause,
+  });
+
+  /// Convierte el modelo en una respuesta json serializable
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'start_timestamp': startTimestamp,
+      'end_timestamp': endTimestamp,
+      'location': location,
+      'event_type': evenType,
+      'fail_cause': failCause,
+    };
+  }
+}
